0295ea60d7ebeb8318e95fe1d1ed96415ada7b77
Faizan Ahmed <faizanahmed14877@gmail.com>
2025-01-03 12:43:30 +0500

    chatgroup and groupmessages model created

--- a_core/urls.py
+++ a_core/urls.py
@@ -24,11 +24,13 @@ from a_home.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('a_home.urls')),
+    path('', include('a_rtchat.urls')),
     path('profile/', include('a_users.urls')),
     path('@<username>/', profile_view, name="profile"),
 ]
 
 # Only used when DEBUG=True, whitenoise can serve files when DEBUG=False
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
